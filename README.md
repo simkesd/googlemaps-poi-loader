961823043a531b1f92eadba1dec25bc823d49679
Milos Simic <simkesd@gmail.com>
2015-04-04 18:32:50 +0200

    added stuff to readme

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # googlemaps-poi-loader
 Small and simple Library that helps you load your Points Of Interests in kml format on your google map.
+
+### Usage
+
+You must load jquery, in my example I am using version 2.1.3.
+
+HTML structure
+
+```
+<ul class="filters">
+    <li data-layer-url="http://happyfist.co/pois/pois/POI/gyms.kml"><a>Gyms</a></li>
+    <li data-layer-url="http://happyfist.co/pois/pois/POI/hotels.kml"><a>Hotels</a></li>
+</ul>
+```
+Javascript
+
+You must initialize map by yourself, and pass reference to the map to constructor of library
+
+```
+var map;
+        function initialize() {
+            map = new google.maps.Map(document.getElementById('map-canvas'), {
+                center: new google.maps.LatLng(40.751077, -73.945135),
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.TERRAIN
+            });
+
+            var gm = new gMap({
+                map: map,
+                filters: $('ul.filters li'),
+                filterClickCallback: function() {
+                    console.log('filterClickCallback');
+                }
+            });
+
+            gm.loadAll();
+            gm.addLayer('http://happyfist.co/pois/pois/POI/hotels.kml');
+            gm.addLayer('http://happyfist.co/pois/pois/POI/gyms.kml');
+
+            console.log(gm.getLayers());
+
+            gm.loadSingleLayer('http://happyfist.co/pois/pois/POI/hotels.kml');
+
+            console.log(gm.getLayers());
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+```
--- src/googlemaps-poi-loader.js
+++ src/googlemaps-poi-loader.js
@@ -7,7 +7,7 @@ var gMap = (function() {
     var filterClickCallback;
 
     /**
-     * Initializes the map and calls the function that creates poly lines.
+     * Uses DOM data passed to constructor to load all POIs to the map
      */
     function loadAll(customOptions) {
         $.each( filters, function( key, value ) {
@@ -27,6 +27,9 @@ var gMap = (function() {
         });
     }
 
+    /**
+     * Removes all previous layers from map and clears all layers from kmlLayer array
+     */
     function clearPreviousLayers() {
         $.each(kmlLayers, function(key, value) {
             value.layerInstance.setMap(null);
@@ -35,9 +38,9 @@ var gMap = (function() {
     }
 
     /**
-     * Adds a KMLLayer based on the URL passed. Clicking on a marker
-     * results in the balloon content being loaded into the right-hand div.
+     * Adds a KMLLayer based on the URL and options passed
      * @param {string} src A URL for a KML file.
+     * @param {object} object with layer options (further reading https://developers.google.com/maps/documentation/javascript/examples/layer-kml)
      */
     function addKmlLayer(src, customOptions) {
         var kmlLayer = loadLayerOnMap(src, customOptions);
@@ -49,6 +52,13 @@ var gMap = (function() {
 //        });
     }
 
+    /**
+     * Loads single layer to map while preserving others
+     *
+     * @param src {string}
+     * @param {object} customOptions (further reading https://developers.google.com/maps/documentation/javascript/examples/layer-kml)
+     * @returns {google.maps.KmlLayer}
+     */
     function loadLayerOnMap(src, customOptions) {
 
         var defaultOptions = {
@@ -70,10 +80,21 @@ var gMap = (function() {
         return kmlLayer;
     }
 
+    /**
+     * Returns all currently loaded layers.
+     *
+     * @returns {Array}
+     */
     function getLayers() {
         return kmlLayers;
     }
 
+    /**
+     * Loads single layer to map while clearing all previously loaded layers
+     *
+     * @param src
+     * @param customOptions
+     */
     function loadSingleLayer(src, customOptions) {
         clearPreviousLayers();
         loadLayerOnMap(src, customOptions);
@@ -97,6 +118,7 @@ var gMap = (function() {
     constructor.prototype.addLayer = addKmlLayer;
     constructor.prototype.getLayers = getLayers;
     constructor.prototype.loadSingleLayer = loadSingleLayer;
+    constructor.prototype.clearPreviousLayers = clearPreviousLayers;
 
     return constructor;
 })();
