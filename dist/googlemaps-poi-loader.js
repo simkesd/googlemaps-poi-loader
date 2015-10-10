da1f406e2b366d517b549c6683bda8590e3bc752
Milos Simic <simkesd@gmail.com>
2015-10-10 16:47:56 +0100

    added gulp module (gulp-remove-code), and improved build task, to delete code meant only for testing

--- dist/googlemaps-poi-loader.js
+++ dist/googlemaps-poi-loader.js
@@ -1,1 +1,167 @@
-var gMap=function(){function e(){return f}function t(e){$.each(u,function(t,r){n($(r).data("layerUrl"),e)})}function r(){$(u).on("click",function(){if(c)for(var e=0;e<f.length;e++){var t=!1;f[e].src===$(this).data("layerUrl")&&(t=!0,a(e))}else o();void 0!==p&&p(),t||n($(this).data("layerUrl"))})}function a(e){f[e].layerInstance.setMap(null),f.splice(e,1)}function o(){$.each(f,function(e,t){t.layerInstance.setMap(null)}),f=[]}function n(e,t){l(e,t)}function l(e,t){var r={url:e,preserveViewport:!0,map:i},a={};$.extend(!0,a,r,t);var o=new google.maps.KmlLayer(a);return f.push({layerInstance:o,src:e}),o}function s(e,t){o(),l(e,t)}var i,p,c,u={},f=[],y=function(e){if(void 0===e.map)throw new Error("Google map object must be passed as property of data parameter.");if(void 0===e.filters)throw new Error("Array of DOM elements containing data-layer-url attribute must be passed as property of data parameter.");i=e.map,u=e.filters,p=e.filterClickCallback,c=e.onLayerChangeKeepState||!1,(e.setEvents===!0||void 0===e.setEvents)&&r(),e.showOnLoad&&t(),console.log(e)};return y.prototype.loadAll=t,y.prototype.addLayer=n,y.prototype.getLayers=e,y.prototype.loadSingleLayer=s,y.prototype.setEvents=r,y.prototype.clearPreviousLayers=o,y}();
+var kmlLoader = (function() {
+    var map,
+        filters = {},
+        kmlLayers = [],
+        filterClickCallback,
+        onLayerChangeKeepState = false,
+        showOnLoad = false
+
+    /**
+     * Returns all currently loaded KML layers.
+     *
+     * @returns {Array}
+     */
+    function getLayers() {
+        return kmlLayers;
+    }
+
+    /**
+     * Uses DOM data passed to constructor to load all KMLs to the map
+     */
+    function loadAll(customOptions) {
+        $.each(filters, function(key, value) {
+            addKmlLayer($(value).data('layerUrl'), customOptions);
+        });
+    }
+
+    /**
+     * Sets events for clicking on kml layer element. If onLayerChangeKeepState is true, then
+     * all layers will be cleared on kml layer element click. The function also check if kml
+     * layer is already loaded. If it is, then it will remove it, if it's not, it will load it.
+     * If you passed filterClickCallback as param, it will execute before the function exits.
+     */
+    function setEvents() {
+        $(filters).on('click', function() {
+            if(!onLayerChangeKeepState) {
+                clearPreviousLayers();
+            }else {
+                var layerLoaded = false;
+                for(var i = 0; i < kmlLayers.length; i++) {
+                    if(kmlLayers[i].src === $(this).data('layerUrl')) {
+                        layerLoaded = true;
+                        removeLayer(i);
+                    }
+                }
+            }
+            if(!layerLoaded) {
+                addKmlLayer($(this).data('layerUrl'));
+            }
+            if(filterClickCallback !== undefined) {
+                filterClickCallback();
+            }
+        });
+    }
+
+    /**
+     * Removes single layer from map and removes it from kmlLayers array
+     */
+    function removeLayer(index) {
+        kmlLayers[index].layerInstance.setMap(null);
+        kmlLayers.splice(index, 1);
+    }
+
+    /**
+     * Removes all previous layers from map and clears all layers from kmlLayer array
+     */
+    function clearPreviousLayers() {
+        $.each(kmlLayers, function(key, value) {
+            value.layerInstance.setMap(null);
+        });
+        kmlLayers = [];
+    }
+
+    /**
+     * Adds a KMLLayer based on the URL and options passed
+     * @param {string} src A URL for a KML file.
+     * @param {object} object with layer options (further reading https://developers.google.com/maps/documentation/javascript/examples/layer-kml)
+     */
+    function addKmlLayer(src, customOptions) {
+        var kmlLayer = loadLayerOnMap(src, customOptions);
+
+//        google.maps.event.addListener(kmlLayer, 'click', function(event) {
+//            var content = event.featureData.infoWindowHtml;
+//            var testimonial = document.getElementById('capture');
+//            testimonial.innerHTML = content;
+//        });
+    }
+
+    /**
+     * Loads single layer to map while preserving others
+     *
+     * @param src {string}
+     * @param {object} customOptions (further reading https://developers.google.com/maps/documentation/javascript/examples/layer-kml)
+     * @returns {google.maps.KmlLayer}
+     */
+    function loadLayerOnMap(src, customOptions) {
+
+        var defaultOptions = {
+            url: src,
+            preserveViewport: true,
+            map: map
+        };
+
+        var finalOptions = {};
+        $.extend(true, finalOptions, defaultOptions, customOptions);
+
+        var kmlLayer = new google.maps.KmlLayer(finalOptions);
+
+        kmlLayers.push({
+            layerInstance: kmlLayer,
+            src: src
+        });
+
+        return kmlLayer;
+    }
+
+    /**
+     * Loads single layer to map while clearing all previously loaded layers
+     *
+     * @param src
+     * @param customOptions
+     */
+    function loadSingleLayer(src, customOptions) {
+        clearPreviousLayers();
+        loadLayerOnMap(src, customOptions);
+    }
+
+    var constructor = function PoiLoader(data) {
+        if(data == undefined) {
+            throw new Error('You must pass parameters to kmlLoader object.');
+        }
+
+        if(data.map === undefined) {
+            throw new Error('Google map object must be passed as property of data parameter.')
+        }
+        if(data.filters === undefined) {
+            throw new Error('Array of DOM elements containing data-layer-url attribute must be passed as property of data parameter.')
+        }
+
+        map = data.map;
+        filters = data.filters;
+        filterClickCallback = data.filterClickCallback;
+        onLayerChangeKeepState = data.onLayerChangeKeepState || onLayerChangeKeepState;
+        showOnLoad = data.showOnLoad;
+
+        if(data.setEvents === true || data.setEvents === undefined) {
+            setEvents();
+        }
+
+        if(showOnLoad) {
+            loadAll();
+        }
+    };
+
+    constructor.prototype.loadAll = loadAll;
+    constructor.prototype.addLayer = addKmlLayer;
+    constructor.prototype.getLayers = getLayers;
+    constructor.prototype.loadSingleLayer = loadSingleLayer;
+    constructor.prototype.setEvents = setEvents;
+    constructor.prototype.clearPreviousLayers = clearPreviousLayers;
+
+    /* start-test-block */
+
+    /* end-test-block */
+
+
+    return constructor;
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var stripCode = require('gulp-strip-code');
+var removeCode = require('gulp-remove-code');
 
-gulp.task('default', function() {
-    gulp.src('src/*.js')
+
+gulp.task('build', function() {
+    /*
+     * Remove code for testing
+     * Compress and minify src file.
+     */
+
+    gulp.src('./src/*.js')
+        .pipe(removeCode({ noDevFeatures: true, onlyForTesting: true}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('./dist/'));
 });
